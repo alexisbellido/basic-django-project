b65e80c9e82b669d9bf0453687afffa3810c747c
Alexis Bellido <alexis@ventanazul.com>
2015-09-14 16:29:08 +0000

    testing ip

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return HttpResponse("Hello, world. You're at the polls index. IP: " + ip)
