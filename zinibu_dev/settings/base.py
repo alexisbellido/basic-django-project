f7e5a314d9ec21a21cbbc13b1a83bbc7011851bb
Alexis Bellido <alexis@ventanazul.com>
2015-10-29 16:42:50 +0000

    use Unipath for static directory and test sqlite db

--- zinibu_dev/settings/base.py
+++ zinibu_dev/settings/base.py
@@ -103,4 +103,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/vagrant/zinibu_dev/static'
+STATIC_ROOT = BASE_DIR.child('static')
