1ecaafaccf6dd10646a4d4d9dd3fd40c68b467b6
Alexis Bellido <alexis@ventanazul.com>
2015-11-13 15:25:11 -0500

    set database settings based on environment variables

--- zinibu_dev/settings/local.py
+++ zinibu_dev/settings/local.py
@@ -2,21 +2,21 @@ from .base import *
 
 DEBUG = True
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, get_env_variable("PROJECT_DATABASES_DEFAULT_NAME")),
-#    }
-#}
-
-# TODO get from env vars
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db1',
-        'USER': 'user1',
-        'PASSWORD': 'user_secret',
-        'HOST': '192.168.1.10',
-        'PORT': '5432',
+if get_env_variable('PROJECT_DATABASES_ENGINE') == 'sqlite3':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, get_env_variable('PROJECT_DATABASES_DEFAULT_NAME')),
+        }
     }
-}
+elif get_env_variable('PROJECT_DATABASES_ENGINE') == 'postgresql':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': get_env_variable('PROJECT_DATABASES_DEFAULT_NAME'),
+            'USER': get_env_variable('PROJECT_DATABASES_DEFAULT_USER'),
+            'PASSWORD': get_env_variable('PROJECT_DATABASES_DEFAULT_PASSWORD'),
+            'HOST': get_env_variable('PROJECT_DATABASES_DEFAULT_HOST'),
+            'PORT': get_env_variable('PROJECT_DATABASES_DEFAULT_PORT'),
+        }
+    }
