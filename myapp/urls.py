c33261dfc5b6d6fb1b0345082a47f8ada1ac5941
Alexis Bellido <alexis@ventanazul.com>
2015-12-08 17:27:13 -0500

    use AppConfig instance

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^appcheck/$', views.appcheck, name='appcheck'),
+    url(r'^$', views.index, name='myapp-index'),
+    url(r'^appcheck/$', views.appcheck, name='myapp-appcheck'),
 ]
